dd496f7ef5c2440682ac29b0e29cf3e2f31dc6d2
Shravan Chaturvedi <142566979+Shravanch19@users.noreply.github.com>
2024-05-25 12:04:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeToggle = document.getElementById('darkModeToggle');
-    const body = document.body;
+const darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else {
+        darkModeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+});
 
-    darkModeToggle.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-        const icon = darkModeToggle.querySelector('i');
-        if (body.classList.contains('dark-mode')) {
-            icon.classList.remove('fa-moon');
-            icon.classList.add('fa-sun');
-        } else {
-            icon.classList.remove('fa-sun');
-            icon.classList.add('fa-moon');
+// Alert for Tile Click
+document.querySelectorAll('.TileLink').forEach(tile => {
+    tile.addEventListener('click', (event) => {
+        event.preventDefault();
+        const userConfirmed = confirm('This will download a torrent file, not the actual one. Do you want to continue?');
+        if (userConfirmed) {
+            window.location.href = tile.href;
         }
     });
 });
