b595ca0b3d5bc023de4cc2f0308b62577e4d7aca
Shravan Chaturvedi <142566979+Shravanch19@users.noreply.github.com>
2024-06-01 12:14:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
-        darkModeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
-    } else {
-        darkModeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
-    }
-});
-
-// Alert for Tile Click
-document.querySelectorAll('.TileLink').forEach(tile => {
-    tile.addEventListener('click', (event) => {
-        event.preventDefault();
-        const userConfirmed = confirm('This will download a torrent file, not the actual one. Do you want to continue?');
-        if (userConfirmed) {
-            window.location.href = tile.href;
-        }
-    });
+document.getElementById('darkModeToggle').addEventListener('click', function() {
+    document.querySelector('main').classList.toggle('dark-mode');
 });
