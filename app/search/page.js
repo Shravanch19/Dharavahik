8d08dcd247f7f5da86abdbb1515849ed3d8d2ad3
Shravan Chaturvedi <skcchat1908@gmail.com>
2024-07-15 21:38:11 +0530

    updated

--- app/search/page.js
+++ app/search/page.js
@@ -1,11 +1,14 @@
+import React, { Suspense } from 'react';
 import Full_Header from "@/components/Full_Header"
 import SearchPG from "@/components/Search_pg/SearchPG"
 
 const page = () => {
   return (
     <div>
-    <Full_Header />
-    <SearchPG />
+      <Full_Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchPG />
+      </Suspense>
     </div>
   )
 }
