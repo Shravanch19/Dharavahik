61d50e14355e520616966448664ce303fd823417
Shravan Chaturvedi <skcchat1908@gmail.com>
2024-07-16 12:03:19 +0530

    analytics updated

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "Dharavahik",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         {children}
         <Footer />
       </body>
